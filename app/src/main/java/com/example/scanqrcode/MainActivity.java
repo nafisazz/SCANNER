00ba7f630e29b27e6decc10d954a1ecf05261397
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-06-30 22:57:29 +0800

    show navbar

--- app/src/main/java/com/example/scanqrcode/MainActivity.java
+++ app/src/main/java/com/example/scanqrcode/MainActivity.java
@@ -55,11 +55,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Fungsi untuk menyembunyikan navbar
-
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
 
         setContentView(R.layout.activity_main);
