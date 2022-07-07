c8072770e442db2d376a18390467a83378c7c1ca
nafisaaz <nafisaazzahra25@gmail.com>
2022-07-07 14:14:35 +0700

    SELESAI❣️❣️❣️❣️❣️

--- app/src/main/java/com/example/Nafisa_0834/ServerAPI.java
+++ app/src/main/java/com/example/Nafisa_0834/ServerAPI.java
@@ -1,6 +1,6 @@
 package com.example.Nafisa_0834;
 
 public class ServerAPI {
-    public static String DATA_API = "http://192.168.11.92/";
-    public static String URL = "http://192.168.11.92/qrcode/qr/";
+    public static String DATA_API = "http://192.168.238.178/";
+    public static String URL = "http://192.168.238.178/qrcode/qr/";
 }
