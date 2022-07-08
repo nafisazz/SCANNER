bbb5fa95c5c6618aa0dc568647569d68f4def35b
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2022-07-08 08:28:50 +0700

    redesign menu utama

--- app/src/main/java/com/example/Nafisa_0834/MenuUtamaActivity.java
+++ app/src/main/java/com/example/Nafisa_0834/MenuUtamaActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class MenuUtamaActivity extends AppCompatActivity {
@@ -15,9 +16,21 @@ public class MenuUtamaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_utama);
+        hideNavigationBar();
 
         initlize();
         onClick();
+    }
+
+    private void hideNavigationBar() {
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
     }
 
     private void onClick() {
